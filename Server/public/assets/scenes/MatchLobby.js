1b1602b23e13d5e681d96980d71cf359faf0214a
Junil8 <just-julian@hotmail.com>
2021-03-03 14:56:23 -0800

    yht
    hgng

--- Server/public/assets/scenes/MatchLobby.js
+++ Server/public/assets/scenes/MatchLobby.js
@@ -20,24 +20,36 @@ export class MatchLobby extends Phaser.Scene {
 
         let centerX = this.cameras.main.width / 2;
 
+        let textTeam1 = this.add.text(50,25,'Team 1', { font: "bold 24px monospace"});
+        let textTeam2 = this.add.text(450,25,'Team 2', { font: "bold 24px monospace"});
+        let buttonJoin1 = this.Method.CreateButton.call(this, 275, 38, 125, 40 , 'Join', () => { console.log("Start") });
+        let buttonJoin2 = this.Method.CreateButton.call(this, 675, 38, 125, 40 , 'Join', () => { console.log("Start") });
+        let borderTopLeft  = this.add.rectangle(centerX - 200,75,375,3,0x595652);
+        let borderTopRight  = this.add.rectangle(centerX + 200,75,375,3,0x595652);
+        let header = {}
 
 
-        let textlobbyCode = this.add.text(50,390,'Lobby Code: 231289', { font: "bold 24px monospace"});
-        let buttonReturn = this.Method.CreateButton.call(this, 450, 400, 175, 50, 'Return', () => { this.scene.start('MainMenu') });
-        let buttonReady = this.Method.CreateButton.call(this, 650, 400, 175, 50, 'Ready', () => { console.log("Start") });
-        let borderbotleft 
-
-        let Team1 = {
-            teamName: null,
-            joinButton: null,
-            borderTop: null,
-            textcolor: null,
-            colorbutton: null,
-            borderBot: null,
-        };
-
+        // footer
+        let textLobbyCode = this.add.text(50,400,'Lobby Code: 231289', { font: "bold 24px monospace"});
+        let buttonReturn = this.Method.CreateButton.call(this, 525, 410, 125, 40, 'Return', () => { this.scene.start('MainMenu') });
+        let buttonReady = this.Method.CreateButton.call(this, 675, 410, 125, 40 , 'Ready', () => { console.log("Start") });
+        let borderBotLeft  = this.add.rectangle(centerX - 200,375,375,3,0x595652);
+        let borderBotRight  = this.add.rectangle(centerX + 200,375,375,3,0x595652);
         let footer = {}
 
+
+        let Team1 = {
+            
+            player1Sprite: null,
+            player2Sprite: null,
+            player1Checkmark: null,
+            player2Checkmark: null,
+            player1Name:null,
+            player2Name:null,
+            textTeamColor: null,
+            teamColor: null,
+            
+        };
 
 
 
